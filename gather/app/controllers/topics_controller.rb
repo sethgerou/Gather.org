995308a3894d6c54598c352045b72adc3ddad9fd
Seth Gerou <seth.gerou@gmail.com>
2017-05-01 14:29:27 -0700

    partial build of topic show - css not working

--- gather/app/controllers/topics_controller.rb
+++ gather/app/controllers/topics_controller.rb
@@ -1,9 +1,13 @@
 class TopicsController < ActionController::Base
   protect_from_forgery with: :exception
 
-def index
-  @topics = Topic.all
-  render :index
-end
+  def index
+    @topics = Topic.all
+    render :index
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
 
 end
