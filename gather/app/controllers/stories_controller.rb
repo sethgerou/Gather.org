7d9472bb0adfa997536d8e09f0f3325dc2d9a56f
Seth Gerou <seth.gerou@gmail.com>
2017-05-02 11:09:19 -0700

    view articles, stories, and resources

--- gather/app/controllers/stories_controller.rb
+++ gather/app/controllers/stories_controller.rb
@@ -1,3 +1,7 @@
-class StoriesController < ApplicationController
-
+class StoriesController < TopicsController
+  def index
+    @topic = Topic.find(params[:topic_id])
+    @stories = Story.where(topic_id: @topic.id)
+    render :index
+  end
 end
--- gather/app/models/topic.rb
+++ gather/app/models/topic.rb
@@ -1,2 +1,5 @@
 class Topic < ApplicationRecord
+  has_many :articles
+  has_many :stories
+  has_many :resources
 end
